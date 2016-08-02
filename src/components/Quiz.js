617291d4a8ad3e2f54a11e71d00f6ac9b8a75d8e
Mitch Gavan <mitch.gavan@fernwoodfitness.com.au>
2016-08-02 16:32:56 +1000

    Added animation

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import Question from '../components/Question';
 import QuestionCount from '../components/QuestionCount';
 import AnswerOption from '../components/AnswerOption';
@@ -19,16 +20,26 @@ function Quiz(props) {
   }
 
   return (
-    <div className="quiz">
-      <QuestionCount
-        counter={props.questionId}
-        total={props.questionTotal}
-      />
-      <Question content={props.question} />
-      <ul className="answerOptions">
-        {props.answerOptions.map(renderAnswerOptions)}
-      </ul>
-    </div>
+    <ReactCSSTransitionGroup
+      className="quiz"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div key={props.questionId}>
+        <QuestionCount
+          counter={props.questionId}
+          total={props.questionTotal}
+        />
+        <Question content={props.question} />
+        <ul className="answerOptions">
+          {props.answerOptions.map(renderAnswerOptions)}
+        </ul>
+      </div>
+    </ReactCSSTransitionGroup>
   );
 
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,22 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 function Result(props) {
-  
+
   return (
-    <div className="result">
-      You prefer <strong>{props.quizResult}</strong>!
-    </div>
+    <ReactCSSTransitionGroup
+      className="result"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div>
+        You prefer <strong>{props.quizResult}</strong>!
+      </div>
+    </ReactCSSTransitionGroup>
   );
 
 }
